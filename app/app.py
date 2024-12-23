a1622f7e0b2ae8c2fb46da024f9b2c865575b660
hben35 <99895665+hben35@users.noreply.github.com>
2024-12-23 17:52:08 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -37,7 +37,7 @@ def ocr():
 
         # Read text from the image
         #result = reader.readtext(image_path, batch_size=16, workers=4, paragraph=True, text_threshold=0.7, low_text=0.4, link_threshold=0.4)
-        result = reader.readtext(image_path, batch_size=16, workers=4, low_text=0.4)
+        result = reader.readtext(image_path, batch_size=16, workers=4, low_text=0.4, paragraph=True)
         # Convert results to serializable types
         serializable_result = [
             {
