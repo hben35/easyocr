2eaeeb947444ad0bef0a89deb97fb41c021944d8
hben35 <99895665+hben35@users.noreply.github.com>
2024-12-23 17:29:30 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS
 
 # Initialize EasyOCR reader with optimized parameters
-reader = easyocr.Reader(['fr', 'en'], gpu=false, workers=4)
+reader = easyocr.Reader(['fr', 'en'], gpu= "False", workers=4)
 
 @app.route('/ocr', methods=['POST'])
 def ocr():
